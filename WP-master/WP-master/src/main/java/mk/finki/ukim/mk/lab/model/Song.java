b4eed54660392df873111c0edf8ac816a17510d0
Alekso93 <107583467+Alekso93@users.noreply.github.com>
2024-12-22 22:17:22 +0100

    Add files via upload

---
+++ WP-master/WP-master/src/main/java/mk/finki/ukim/mk/lab/model/Song.java
@@ -0,0 +1,43 @@
+package mk.finki.ukim.mk.lab.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Song {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String trackID;
+    private String title;
+    private String genre;
+    private int releaseYear;
+    //private List<Artist> performers;
+    @ManyToOne
+    private Album album;
+
+
+    public Song(String trackID, String title, String genre, int releaseYear, Album album) {
+        this.trackID = trackID;
+        this.title = title;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+        //this.performers = new ArrayList<>();
+        this.album = album;
+    }
+
+    public void addArtist(Artist a) {
+//        if (!performers.contains(a)) {
+//            performers.add(a);
+//        }
+    }
+
+    public void addAlbum(Album a) {
+        if (this.album == null) {
+            this.album = a;
+        }
+    }
+}
---
+++ WP-master/WP-master/src/main/java/mk/finki/ukim/mk/lab/service/SongService.java
@@ -0,0 +1,19 @@
+package mk.finki.ukim.mk.lab.service;
+
+import mk.finki.ukim.mk.lab.model.Album;
+import mk.finki.ukim.mk.lab.model.Artist;
+import mk.finki.ukim.mk.lab.model.Song;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SongService {
+    List<Song> listSongs();
+    Artist addArtistToSong(Artist artist, Song song);
+    Song findByTrackId(String trackId);
+    Optional<Song> findById(Long id);
+    void deleteById(Long id);
+    Optional<Song> save(String trackID, String title, String genre, int releaseYear, Long albumID);
+    Optional<Song> update(Long id, String trackID, String title, String genre, int releaseYear, Long albumID);
+    List<Song> findAllByAlbum_Id(Long albumId);
+}
